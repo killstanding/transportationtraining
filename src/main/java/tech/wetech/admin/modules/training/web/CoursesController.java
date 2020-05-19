fe9dce71613fda7cdbd8682e65b4e9bbf9aa0765
killstanding <w5743216@163.com>
2020-05-19 14:35:19 +0800

    课程courses页面的session中增加课程类型和课程负责人

--- src/main/java/tech/wetech/admin/modules/training/web/CoursesController.java
+++ src/main/java/tech/wetech/admin/modules/training/web/CoursesController.java
@@ -14,8 +14,12 @@ import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.core.utils.Result;
 import tech.wetech.admin.modules.base.query.PageQuery;
 import tech.wetech.admin.modules.base.web.BaseCrudController;
+import tech.wetech.admin.modules.system.service.UserService;
 import tech.wetech.admin.modules.training.po.Courses;
+import tech.wetech.admin.modules.training.po.PubCode;
 import tech.wetech.admin.modules.training.service.CoursesService;
+import tech.wetech.admin.modules.training.service.PubCodeService;
+
 import java.util.Date;
 import java.util.List;
 
@@ -27,56 +31,64 @@ import javax.validation.constraints.NotNull;
 public class CoursesController extends BaseCrudController<Courses> {
 
 	@Autowired
-    private CoursesService service;
-    
-    @GetMapping
-    @RequiresPermissions("courses:view")
-    public String userPage(Model model) {
-        return "system/courses";
-    }
-    
-    @ResponseBody
-    @GetMapping("/list")
-    @RequiresPermissions("courses:view")
-    @Override
-    public Result<List<Courses>> queryList(Courses entity, PageQuery pageQuery) {
-        Page<Courses> page = (Page<Courses>) service.queryListByLike(entity, pageQuery);
-        return Result.success(page.getResult()).addExtra("total", page.getTotal());
-    }
-    
-    @ResponseBody
-    @PostMapping("/create")
-    //@RequiresPermissions("courses:create")
-    @SystemLog("课程管理课程创建")
-    @Override
-    public Result<String> create(@Validated(Courses.CoursesCreateChecks.class) Courses entity) {
-    	String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
-    	entity.setCreateTime(curTime);
-    	entity.setUpdateTime(curTime);
-    	service.create(entity);
-        return Result.success();
-    }
-  
-    @ResponseBody
-    @PostMapping("/update")
-    @RequiresPermissions("courses:update")
-    @SystemLog("课程管理课程更新")
-    @Override
-    public Result<String> update(@Validated(Courses.CoursesUpdateChecks.class) Courses entity) {
-    	String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
-    	entity.setUpdateTime(curTime);
-    	service.updateNotNull(entity);
-        return Result.success();
-    }
+	private CoursesService service;
+	@Autowired
+	private UserService userService;
+	@Autowired
+    private PubCodeService pubCodeService;
+	
+	@GetMapping
+	@RequiresPermissions("courses:view")
+	public String userPage(Model model) {
+		PubCode pubCode = new PubCode();
+    	pubCode.setPubType("course_type");
+    	model.addAttribute("courseTypeList", pubCodeService.queryList(pubCode));
+    	model.addAttribute("userList", userService.queryAll());
+		return "system/courses";
+	}
 
-    @ResponseBody
-    @PostMapping("/delete-batch")
-    @RequiresPermissions("courses:delete")
-    @SystemLog("课程管理课程删除")
-    @Override
-    public Result<String> deleteBatchByIds(@NotNull @RequestParam("id") Object[] ids) {
-        super.deleteBatchByIds(ids);
-        return Result.success();
-    }
+	@ResponseBody
+	@GetMapping("/list")
+	@RequiresPermissions("courses:view")
+	@Override
+	public Result<List<Courses>> queryList(Courses entity, PageQuery pageQuery) {
+		Page<Courses> page = (Page<Courses>) service.queryListByLike(entity, pageQuery);
+		return Result.success(page.getResult()).addExtra("total", page.getTotal());
+	}
+
+	@ResponseBody
+	@PostMapping("/create")
+	//@RequiresPermissions("courses:create")
+	@SystemLog("课程管理课程创建")
+	@Override
+	public Result<String> create(@Validated(Courses.CoursesCreateChecks.class) Courses entity) {
+		String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
+		entity.setCreateTime(curTime);
+		entity.setUpdateTime(curTime);
+		service.create(entity);
+		return Result.success();
+	}
+
+	@ResponseBody
+	@PostMapping("/update")
+	@RequiresPermissions("courses:update")
+	@SystemLog("课程管理课程更新")
+	@Override
+	public Result<String> update(@Validated(Courses.CoursesUpdateChecks.class) Courses entity) {
+		String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
+		entity.setUpdateTime(curTime);
+		service.updateNotNull(entity);
+		return Result.success();
+	}
+
+	@ResponseBody
+	@PostMapping("/delete-batch")
+	@RequiresPermissions("courses:delete")
+	@SystemLog("课程管理课程删除")
+	@Override
+	public Result<String> deleteBatchByIds(@NotNull @RequestParam("id") Object[] ids) {
+		super.deleteBatchByIds(ids);
+		return Result.success();
+	}
 
 }
