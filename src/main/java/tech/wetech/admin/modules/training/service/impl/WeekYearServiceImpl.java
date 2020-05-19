7729f13c9930d60d1a7f621b4ce696e92e6ff81d
killstanding <w5743216@163.com>
2020-05-19 16:52:57 +0800

    添加service注解，解决项目无法启动问题

--- src/main/java/tech/wetech/admin/modules/training/service/impl/WeekYearServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/WeekYearServiceImpl.java
@@ -1,8 +1,14 @@
 package tech.wetech.admin.modules.training.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import tech.wetech.admin.modules.base.service.impl.BaseService;
+import tech.wetech.admin.modules.training.mapper.WeekYearMapper;
 import tech.wetech.admin.modules.training.po.WeekYear;
 import tech.wetech.admin.modules.training.service.WeekYearService;
-
+@Service
 public class WeekYearServiceImpl extends BaseService<WeekYear> implements WeekYearService {
+	@Autowired
+	private WeekYearMapper mapper;
 }
