ec9bd6d74f9dce68e687ac1ddd50641b248f70a8
killstanding <w5743216@163.com>
2020-05-30 19:04:09 +0800

    补充注解servcie

--- src/main/java/tech/wetech/admin/modules/training/service/impl/CourseArrangementReToolServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/CourseArrangementReToolServiceImpl.java
@@ -1,12 +1,14 @@
 package tech.wetech.admin.modules.training.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tech.wetech.admin.modules.base.service.impl.BaseService;
 import tech.wetech.admin.modules.training.mapper.CourseArrangementReToolMapper;
 import tech.wetech.admin.modules.training.po.CourseArrangementReTool;
 import tech.wetech.admin.modules.training.service.CourseArrangementReToolService;
 
+@Service
 public class CourseArrangementReToolServiceImpl extends BaseService<CourseArrangementReTool> implements CourseArrangementReToolService {
     @Autowired
     private CourseArrangementReToolMapper mapper;
--- src/main/java/tech/wetech/admin/modules/training/service/impl/StudentServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/StudentServiceImpl.java
@@ -1,11 +1,14 @@
 package tech.wetech.admin.modules.training.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import tech.wetech.admin.modules.base.service.impl.BaseService;
 import tech.wetech.admin.modules.training.mapper.StudentMapper;
 import tech.wetech.admin.modules.training.po.Student;
 import tech.wetech.admin.modules.training.service.StudentService;
 
+@Service
 public class StudentServiceImpl extends BaseService<Student> implements StudentService {
     @Autowired
     private StudentMapper mapper;
