d66cf2c30e0187cd75fab06e82a09b0cff5a4b10
killstanding <w5743216@163.com>
2020-05-31 14:17:04 +0800

    维修申请记录自动生成编号字段数据

--- src/main/java/tech/wetech/admin/modules/training/service/impl/MaintenanceRecordServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/MaintenanceRecordServiceImpl.java
@@ -1,10 +1,13 @@
 package tech.wetech.admin.modules.training.service.impl;
 
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.modules.base.service.impl.BaseService;
+import tech.wetech.admin.modules.system.common.CommonVariable;
 import tech.wetech.admin.modules.training.mapper.MaintenanceRecordMapper;
 import tech.wetech.admin.modules.training.po.MaintenanceRecord;
 import tech.wetech.admin.modules.training.po.StatusCountResult;
@@ -21,4 +24,19 @@ public class MaintenanceRecordServiceImpl extends BaseService<MaintenanceRecord>
 		return mapper.selectCountNumGourpByStatus();
 	}
 	
+	@Override
+	public int create(MaintenanceRecord entity) {
+		// TODO Auto-generated method stub
+		int k = super.create(entity);
+		if(entity.getRecordId().equals("")){
+			//更新编号
+			MaintenanceRecord record = new MaintenanceRecord();
+	    	record.setId(entity.getId());
+	    	String dateStr = DateUtil.dateToStr(new Date(), DateUtil.DATE_FORMATE_SIMPLE);
+	    	String code = CommonVariable.getNumberPrefix(CommonVariable.NUMBER_PREFIX_MAINTENANCE_RECORD, dateStr, record.getId());
+	    	record.setRecordId(code);
+	    	super.updateNotNull(record);
+		}
+		return k;
+	}
 }
