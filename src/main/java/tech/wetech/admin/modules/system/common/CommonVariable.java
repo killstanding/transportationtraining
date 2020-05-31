b1df80fa0d61a82c9af466d186381c5a20574e67
killstanding <w5743216@163.com>
2020-05-31 14:10:58 +0800

    设备自动生成编号字段数据

--- src/main/java/tech/wetech/admin/modules/system/common/CommonVariable.java
+++ src/main/java/tech/wetech/admin/modules/system/common/CommonVariable.java
@@ -1,5 +1,6 @@
 package tech.wetech.admin.modules.system.common;
 
+
 public class CommonVariable {
 	//默认角色编号
 	public static final int TRAINING_ROOM_ADMIN_ROLE_ID = 7;//实训室管理员角色编号
@@ -15,6 +16,27 @@ public class CommonVariable {
 	public static final int TEACHER_ROLE_ID = 9;//教师角色编号
 	
 	public static final int DIRECTOR_ROLE_ID = 10;//主任角色编号
+	
+	//编号前缀
+	public static final String NUMBER_PREFIX_DEVICE = "DEV";//设备编号前缀
+	
+	public static final String NUMBER_PREFIX_TRAINING_ROOM = "TRA";//实训室编号前缀
+	
+	public static final String NUMBER_PREFIX_CONSUMABLES = "CON";//耗材编号前缀
+	
+	public static final String NUMBER_PREFIX_TOOLS = "T";//工具编号前缀
+	
+	public static final String NUMBER_PREFIX_INSPECTION_RECORD = "IR";//巡检记录编号前缀
+	
+	public static final String NUMBER_PREFIX_MAINTENANCE_RECORD = "MR";//维修申请单编号前缀
+	
+	public static final String NUMBER_PREFIX_Collection_RECORD = "CR";//领用单编号前缀
+	
+	public static String getNumberPrefix(String numberPrefix,String dateStr,int id){
+		StringBuilder sb = new StringBuilder();
+		sb.append(numberPrefix).append(dateStr).append(id);
+		return sb.toString();
+	}
 	
 }
 
--- src/main/java/tech/wetech/admin/modules/training/service/impl/AssetServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/AssetServiceImpl.java
@@ -1,9 +1,13 @@
 package tech.wetech.admin.modules.training.service.impl;
 
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.modules.base.service.impl.BaseService;
+import tech.wetech.admin.modules.system.common.CommonVariable;
 import tech.wetech.admin.modules.training.mapper.AssetMapper;
 import tech.wetech.admin.modules.training.po.Asset;
 import tech.wetech.admin.modules.training.po.StatusCountResult;
@@ -26,4 +30,22 @@ public class AssetServiceImpl extends BaseService<Asset> implements AssetService
 		// TODO Auto-generated method stub
 		mapper.updateCodeById(record);
 	}
+
+	@Override
+	public int create(Asset entity) {
+		// TODO Auto-generated method stub
+		int k = super.create(entity);
+		if(entity.getAssetCode().equals("")){
+			//更新编号
+	    	Asset record = new Asset();
+	    	record.setId(entity.getId());
+	    	String dateStr = DateUtil.dateToStr(new Date(), DateUtil.DATE_FORMATE_SIMPLE);
+	    	String code = CommonVariable.getNumberPrefix(CommonVariable.NUMBER_PREFIX_DEVICE, dateStr, record.getId());
+	    	record.setAssetCode(code);
+	    	super.updateNotNull(record);
+		}
+		return k;
+	}
+	
+	
 }
