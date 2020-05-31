db8e9a9023c3c77166ebc7798a4a23d491bfe7f2
killstanding <w5743216@163.com>
2020-05-31 14:20:32 +0800

    工具自动生成编号字段数据

--- src/main/java/tech/wetech/admin/modules/training/service/impl/ToolsServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/ToolsServiceImpl.java
@@ -1,9 +1,13 @@
 package tech.wetech.admin.modules.training.service.impl;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.modules.base.service.impl.BaseService;
+import tech.wetech.admin.modules.system.common.CommonVariable;
 import tech.wetech.admin.modules.training.mapper.ToolsMapper;
 import tech.wetech.admin.modules.training.po.Tools;
 import tech.wetech.admin.modules.training.service.ToolsService;
@@ -18,5 +22,21 @@ public class ToolsServiceImpl extends BaseService<Tools> implements ToolsService
 	public void updateCodeById(Tools record) {
 		// TODO Auto-generated method stub
 		mapper.updateCodeById(record);
+	}
+	
+	@Override
+	public int create(Tools entity) {
+		// TODO Auto-generated method stub
+		int k = super.create(entity);
+		if(entity.getAssetCode().equals("")){
+			//更新编号
+			Tools record = new Tools();
+	    	record.setId(entity.getId());
+	    	String dateStr = DateUtil.dateToStr(new Date(), DateUtil.DATE_FORMATE_SIMPLE);
+	    	String code = CommonVariable.getNumberPrefix(CommonVariable.NUMBER_PREFIX_TOOLS, dateStr, record.getId());
+	    	record.setAssetCode(code);
+	    	super.updateNotNull(record);
+		}
+		return k;
 	}
 }
