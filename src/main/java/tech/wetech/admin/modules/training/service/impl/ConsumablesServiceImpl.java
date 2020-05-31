ba4776e314f0c7697675b67f8ad0d2177588c517
killstanding <w5743216@163.com>
2020-05-31 14:20:04 +0800

    耗材自动生成编号字段数据

--- src/main/java/tech/wetech/admin/modules/training/service/impl/ConsumablesServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/ConsumablesServiceImpl.java
@@ -1,9 +1,13 @@
 package tech.wetech.admin.modules.training.service.impl;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.modules.base.service.impl.BaseService;
+import tech.wetech.admin.modules.system.common.CommonVariable;
 import tech.wetech.admin.modules.training.mapper.ConsumablesMapper;
 import tech.wetech.admin.modules.training.po.Consumables;
 import tech.wetech.admin.modules.training.service.ConsumablesService;
@@ -17,5 +21,21 @@ public class ConsumablesServiceImpl extends BaseService<Consumables> implements 
 	@Override
 	public void updateCodeById(Consumables record) {
 		mapper.updateCodeById(record);
+	}
+	
+	@Override
+	public int create(Consumables entity) {
+		// TODO Auto-generated method stub
+		int k = super.create(entity);
+		if(entity.getConsumablesCode().equals("")){
+			//更新编号
+			Consumables record = new Consumables();
+	    	record.setId(entity.getId());
+	    	String dateStr = DateUtil.dateToStr(new Date(), DateUtil.DATE_FORMATE_SIMPLE);
+	    	String code = CommonVariable.getNumberPrefix(CommonVariable.NUMBER_PREFIX_CONSUMABLES, dateStr, record.getId());
+	    	record.setConsumablesCode(code);
+	    	super.updateNotNull(record);
+		}
+		return k;
 	}
 }
