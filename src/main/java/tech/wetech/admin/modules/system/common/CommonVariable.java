8e257f72332b27bfc9f76840a14b40283f83bc77
killstanding <w5743216@163.com>
2020-05-31 10:35:27 +0800

    添加角色默认编号的常量类

---
+++ src/main/java/tech/wetech/admin/modules/system/common/CommonVariable.java
@@ -0,0 +1,20 @@
+package tech.wetech.admin.modules.system.common;
+
+public class CommonVariable {
+	//默认角色编号
+	public static final int TRAINING_ROOM_ADMIN_ROLE_ID = 7;//实训室管理员角色编号
+	
+	public static final int DEVICE_ADMIN_ROLE_ID = 8;//设备管理员角色编号
+	
+	public static final int CONSUMABLES_ADMIN_ROLE_ID = 11;//耗材管理员角色编号
+	
+	public static final int TOOL_ADMIN_ROLE_ID = 12;//工具管理员角色编号
+	
+	public static final int COURSES_ADMIN_ROLE_ID = 13;//课程管理员角色编号
+	
+	public static final int TEACHER_ROLE_ID = 9;//教师角色编号
+	
+	public static final int DIRECTOR_ROLE_ID = 10;//主任角色编号
+	
+}
+
