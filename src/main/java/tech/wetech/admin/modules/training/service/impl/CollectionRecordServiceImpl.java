4ae6a14f0d1954937b17632d3af8e69cab14124e
killstanding <w5743216@163.com>
2020-05-31 14:12:50 +0800

    领用记录自动生成编号字段数据

--- src/main/java/tech/wetech/admin/modules/training/service/impl/CollectionRecordServiceImpl.java
+++ src/main/java/tech/wetech/admin/modules/training/service/impl/CollectionRecordServiceImpl.java
@@ -1,10 +1,16 @@
 package tech.wetech.admin.modules.training.service.impl;
 
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.modules.base.service.impl.BaseService;
+import tech.wetech.admin.modules.system.common.CommonVariable;
 import tech.wetech.admin.modules.training.mapper.CollectionRecordMapper;
+import tech.wetech.admin.modules.training.po.Asset;
 import tech.wetech.admin.modules.training.po.CollectionRecord;
 import tech.wetech.admin.modules.training.service.CollectionRecordService;
 
@@ -13,5 +19,21 @@ public class CollectionRecordServiceImpl extends BaseService<CollectionRecord> i
 	
 	@Autowired
 	private CollectionRecordMapper mapper;
+	
+	@Override
+	public int create(CollectionRecord entity) {
+		// TODO Auto-generated method stub
+		int k = super.create(entity);
+		if(entity.getRecordId().equals("")){
+			//更新编号
+			CollectionRecord record = new CollectionRecord();
+	    	record.setId(entity.getId());
+	    	String dateStr = DateUtil.dateToStr(new Date(), DateUtil.DATE_FORMATE_SIMPLE);
+	    	String code = CommonVariable.getNumberPrefix(CommonVariable.NUMBER_PREFIX_DEVICE, dateStr, record.getId());
+	    	record.setRecordId(code);
+	    	super.updateNotNull(record);
+		}
+		return k;
+	}
 	
 }
