7fd80528ce940fc3542c086c430bb6657898dfff
killstanding <w5743216@163.com>
2020-06-18 10:41:30 +0800

    删除不必要的文件

--- src/main/resources/tech/wetech/admin/modules/training/mapper/InspectionPlanMapper.java
+++
@@ -1,7 +0,0 @@
-package tech.wetech.admin.modules.training.mapper;
-
-import tech.wetech.admin.core.utils.MyMapper;
-import tech.wetech.admin.modules.training.po.InspectionPlan;
-
-public interface InspectionPlanMapper extends MyMapper<InspectionPlan> {
-}
