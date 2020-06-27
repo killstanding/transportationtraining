5283bcd12d0b03d73aab9f0d1d7ae1108fc5177e
killstanding <w5743216@163.com>
2020-06-27 10:16:38 +0800

    排课  关联工具生成 领用申请记录  删除关系删除领用申请记录

--- src/main/java/tech/wetech/admin/modules/training/web/CourseArrangementReToolController.java
+++ src/main/java/tech/wetech/admin/modules/training/web/CourseArrangementReToolController.java
@@ -14,12 +14,20 @@ import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.core.utils.Result;
 import tech.wetech.admin.modules.base.query.PageQuery;
 import tech.wetech.admin.modules.base.web.BaseCrudController;
+import tech.wetech.admin.modules.system.po.Organization;
+import tech.wetech.admin.modules.system.service.OrganizationService;
 import tech.wetech.admin.modules.training.po.AssetClassification;
+import tech.wetech.admin.modules.training.po.CollectionRecord;
+import tech.wetech.admin.modules.training.po.Consumables;
+import tech.wetech.admin.modules.training.po.CourseArrangement;
 import tech.wetech.admin.modules.training.po.CourseArrangementReTool;
 import tech.wetech.admin.modules.training.po.PubCode;
+import tech.wetech.admin.modules.training.po.Tools;
 import tech.wetech.admin.modules.training.service.AssetClassificationService;
+import tech.wetech.admin.modules.training.service.CollectionRecordService;
 import tech.wetech.admin.modules.training.service.ToolsService;
 import tech.wetech.admin.modules.training.service.CourseArrangementReToolService;
+import tech.wetech.admin.modules.training.service.CourseArrangementService;
 import tech.wetech.admin.modules.training.service.PubCodeService;
 import java.util.Date;
 import java.util.List;
@@ -38,6 +46,12 @@ public class CourseArrangementReToolController extends BaseCrudController<Course
     private AssetClassificationService assetClassificationService;
 	@Autowired
     private PubCodeService pubCodeService;
+	@Autowired
+    private CollectionRecordService collectionRecordService;
+	@Autowired
+	private CourseArrangementService courseArrangementService;
+	@Autowired
+    private OrganizationService organizationService;
 	
     @GetMapping
     @RequiresPermissions("coursearrangementretool:view")
@@ -71,6 +85,33 @@ public class CourseArrangementReToolController extends BaseCrudController<Course
     	entity.setCreateTime(curTime);
     	entity.setUpdateTime(curTime);
     	service.create(entity);
+    	
+    	CourseArrangement courseArrangement = courseArrangementService.queryById(entity.getCourseArrangementId());
+    	Tools tools =  toolsService.queryById(entity.getAssetId());
+    	Organization org = organizationService.queryById(courseArrangement.getClassId());
+    	Organization sys = organizationService.queryById(org.getParentId());
+    	CollectionRecord record = new CollectionRecord();
+    	record.setRecordName(tools.getAssetName()+"领用申请");
+    	record.setAssetId(tools.getId());
+    	record.setAssetName(tools.getAssetName());
+    	record.setAssetType("工具");
+    	record.setAssetTypeCode("asset_type_tool");
+    	record.setCollectionMajor(sys.getName());//领用的专业(系)
+    	record.setCollectionMajorId(sys.getId().intValue());//领用的专业(系)编号
+    	record.setCollectionCourse(courseArrangement.getCourseName());//领用的课程
+    	record.setCollectionCourseId(courseArrangement.getCourseId());//领用的课程编号
+    	record.setApplicant(courseArrangement.getLecturerlTeacher());//申请人
+    	record.setApplicantId(courseArrangement.getLecturerlTeacherId());//申请人编号
+    	record.setCollectedQuantity(entity.getNumberOfApplications()+"");//申请数量
+    	//record.setRemark();//备注
+    	record.setCreateYear(DateUtil.dateToStr(new Date(), DateUtil.YEAR_FORMATE));
+    	record.setPendingPerson(tools.getAssetUser());//待处理人
+    	record.setPendingPersonId(Integer.parseInt(tools.getAssetUserId()));//待处理人编号
+    	record.setCreateTime(curTime);
+    	record.setUpdateTime(curTime);
+    	record.setReId(entity.getId());
+    	collectionRecordService.create(record);
+    	
         return Result.success();
     }
   
@@ -93,6 +134,13 @@ public class CourseArrangementReToolController extends BaseCrudController<Course
     @Override
     public Result<String> deleteBatchByIds(@NotNull @RequestParam("id") Object[] ids) {
         super.deleteBatchByIds(ids);
+        for (int i = 0; i < ids.length; i++) {
+        	int reId = (int)ids[i];
+			CollectionRecord entity = new CollectionRecord();
+			entity.setAssetTypeCode("asset_type_tool");
+			entity.setReId(reId);
+			collectionRecordService.delete(entity);
+		}
         return Result.success();
     }
 }
