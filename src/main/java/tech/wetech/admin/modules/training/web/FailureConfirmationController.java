56798a0c7e2c90142cb0813bce61608e7f6eaf3d
Administrator <Administrator@H0FQ3IP2RLQWNJC>
2020-07-02 09:27:52 +0800

    故障确认，设备更新为维修，工具正在维修数量+1,剩余数量-1

--- src/main/java/tech/wetech/admin/modules/training/web/FailureConfirmationController.java
+++ src/main/java/tech/wetech/admin/modules/training/web/FailureConfirmationController.java
@@ -10,6 +10,7 @@ import io.swagger.annotations.Api;
 import tech.wetech.admin.core.annotation.SystemLog;
 import tech.wetech.admin.core.utils.DateUtil;
 import tech.wetech.admin.core.utils.Result;
+import tech.wetech.admin.core.utils.StringUtil;
 import tech.wetech.admin.modules.base.query.PageQuery;
 import tech.wetech.admin.modules.base.web.BaseCrudController;
 import tech.wetech.admin.modules.system.service.OrganizationService;
@@ -18,6 +19,7 @@ import tech.wetech.admin.modules.training.po.Asset;
 import tech.wetech.admin.modules.training.po.AssetClassification;
 import tech.wetech.admin.modules.training.po.FailureConfirmation;
 import tech.wetech.admin.modules.training.po.MaintenanceRecord;
+import tech.wetech.admin.modules.training.po.Tools;
 import tech.wetech.admin.modules.training.service.AssetClassificationService;
 import tech.wetech.admin.modules.training.service.AssetService;
 import tech.wetech.admin.modules.training.service.FailureConfirmationService;
@@ -35,115 +37,136 @@ import javax.validation.constraints.NotNull;
 @RequestMapping("/failureconfirmation")
 public class FailureConfirmationController extends BaseCrudController<FailureConfirmation> {
 
-    @Autowired
-    private FailureConfirmationService service;
 	@Autowired
-    private AssetClassificationService assetClassificationService;
+	private FailureConfirmationService service;
+	@Autowired
+	private AssetClassificationService assetClassificationService;
 	@Autowired
 	private PositionService positionService;
 	@Autowired
-    private OrganizationService organizationService;
-    @Autowired
-    private UserService userService;
+	private OrganizationService organizationService;
 	@Autowired
-    private AssetService assetService;
+	private UserService userService;
+	@Autowired
+	private AssetService assetService;
 	@Autowired
 	private MaintenanceRecordService maintenanceRecordService;
 	@Autowired
-    private ToolsService toolService;
-	
-    @GetMapping
-    @RequiresPermissions("failureconfirmation:view")
-    public String page(Model model) {
-    	AssetClassification assetClassification = new AssetClassification();
-    	assetClassification.setAssetTypeCode("asset_type_device");//设备
-    	model.addAttribute("assetClassificationList", assetClassificationService.queryList(assetClassification));
-    	//model.addAttribute("positionList", positionService.queryAll());
-    	//model.addAttribute("organizationList", organizationService.queryAll());
-    	//model.addAttribute("userList", userService.queryAll());
-    	model.addAttribute("assetList", assetService.queryAll());
-        return "system/failureconfirmation";
-    }
-    
-    @GetMapping("/toolfailureconfirmation")
-    @RequiresPermissions("toolfailureconfirmation:view")
-    public String toolPage(Model model) {
-    	AssetClassification assetClassification = new AssetClassification();
-    	assetClassification.setAssetTypeCode("asset_type_device");//设备
-    	model.addAttribute("assetClassificationList", assetClassificationService.queryList(assetClassification));
-    	//model.addAttribute("positionList", positionService.queryAll());
-    	//model.addAttribute("organizationList", organizationService.queryAll());
-    	//model.addAttribute("userList", userService.queryAll());
-    	model.addAttribute("toolsList", toolService.queryAll());
-        return "system/toolfailureconfirmation";
-    }
-    
-    @ResponseBody
-    @GetMapping("/list")
-    @RequiresPermissions("failureconfirmation:view")
-    @Override
-    public Result<List<FailureConfirmation>> queryList(FailureConfirmation entity, PageQuery pageQuery) {
-        Page<FailureConfirmation> page = (Page<FailureConfirmation>) service.queryList(entity, pageQuery);
-        return Result.success(page.getResult()).addExtra("total", page.getTotal());
-    }
-    
-    @ResponseBody
-    @PostMapping("/create")
-    @RequiresPermissions("failureconfirmation:create")
-    @SystemLog("设备维修故障确认创建")
-    @Override
-    public Result create(@Validated(FailureConfirmation.FailureConfirmationCreateChecks.class) FailureConfirmation entity) {
-    	String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
-    	entity.setCreateTime(curTime);
-    	entity.setUpdateTime(curTime);
-    	service.create(entity);
-    	
-    	//故障确认更新设备状态为维修
-    	updateAssetStatus(entity);
-        return Result.success();
-    }
-  
-    @ResponseBody
-    @PostMapping("/update")
-    @RequiresPermissions("failureconfirmation:update")
-    @SystemLog("设备维修故障确认更新")
-    @Override
-    public Result update(@Validated(FailureConfirmation.FailureConfirmationUpdateChecks.class) FailureConfirmation entity) {
-    	String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
-    	entity.setUpdateTime(curTime);
-    	service.updateNotNull(entity);
-    	//故障确认更新设备状态为维修
-    	updateAssetStatus(entity);
-        return Result.success();
-    }
-    
-    /**
-     * 故障确认更新设备状态为维修
-     * @param entity
-     */
-    private void updateAssetStatus(FailureConfirmation entity){
-    	//故障确认，那么设备状态变为维修
-    	int isRepair = entity.getRepairRequired();//是否需要维修 1 是 0 否
-    	if(isRepair==1){
-    		int recordId = entity.getRecordId();
-    		MaintenanceRecord record = maintenanceRecordService.queryById(recordId);
-    		int assetId = record.getAssetId();
-    		Asset asset = new Asset();
-        	asset.setId(assetId);
-        	asset.setAssetStatus("维修");
-        	asset.setAssetStatusCode("asset_status_repair");
-        	assetService.updateNotNull(asset);
-     	}
-    } 
-    
-    @ResponseBody
-    @PostMapping("/delete-batch")
-    @RequiresPermissions("failureconfirmation:delete")
-    @SystemLog("设备维修故障确认删除")
-    @Override
-    public Result deleteBatchByIds(@NotNull @RequestParam("id") Object[] ids) {
-        super.deleteBatchByIds(ids);
-        return Result.success();
-    }
+	private ToolsService toolService;
+	@Autowired
+	private ToolsService toolsService;
+
+	@GetMapping
+	@RequiresPermissions("failureconfirmation:view")
+	public String page(Model model) {
+		AssetClassification assetClassification = new AssetClassification();
+		assetClassification.setAssetTypeCode("asset_type_device");//设备
+		model.addAttribute("assetClassificationList", assetClassificationService.queryList(assetClassification));
+		//model.addAttribute("positionList", positionService.queryAll());
+		//model.addAttribute("organizationList", organizationService.queryAll());
+		//model.addAttribute("userList", userService.queryAll());
+		model.addAttribute("assetList", assetService.queryAll());
+		return "system/failureconfirmation";
+	}
+
+	@GetMapping("/toolfailureconfirmation")
+	@RequiresPermissions("toolfailureconfirmation:view")
+	public String toolPage(Model model) {
+		AssetClassification assetClassification = new AssetClassification();
+		assetClassification.setAssetTypeCode("asset_type_device");//设备
+		model.addAttribute("assetClassificationList", assetClassificationService.queryList(assetClassification));
+		//model.addAttribute("positionList", positionService.queryAll());
+		//model.addAttribute("organizationList", organizationService.queryAll());
+		//model.addAttribute("userList", userService.queryAll());
+		model.addAttribute("toolsList", toolService.queryAll());
+		return "system/toolfailureconfirmation";
+	}
+
+	@ResponseBody
+	@GetMapping("/list")
+	@RequiresPermissions("failureconfirmation:view")
+	@Override
+	public Result<List<FailureConfirmation>> queryList(FailureConfirmation entity, PageQuery pageQuery) {
+		Page<FailureConfirmation> page = (Page<FailureConfirmation>) service.queryList(entity, pageQuery);
+		return Result.success(page.getResult()).addExtra("total", page.getTotal());
+	}
+
+	@ResponseBody
+	@PostMapping("/create")
+	@RequiresPermissions("failureconfirmation:create")
+	@SystemLog("设备维修故障确认创建")
+	@Override
+	public Result create(@Validated(FailureConfirmation.FailureConfirmationCreateChecks.class) FailureConfirmation entity) {
+		String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
+		entity.setCreateTime(curTime);
+		entity.setUpdateTime(curTime);
+		service.create(entity);
+		int recordId = entity.getRecordId();
+		MaintenanceRecord record = maintenanceRecordService.queryById(recordId);
+		if(record!=null){
+			int assetId = record.getAssetId();
+			String flowTypeCode = record.getFlowTypeCode();
+			switch (flowTypeCode) {
+			case "equipment_maintenance":
+				//故障确认更新设备状态为维修
+				updateAssetStatus(entity);
+				break;
+			case "tools_maintenance":
+				Tools tools = toolsService.queryById(assetId);
+				int repairingQuantity  = StringUtil.strToInt(tools.getRepairingQuantity()) + 1;
+				tools.setRepairingQuantity(repairingQuantity+"");//正在维修数量
+				int remainingQuantity  = StringUtil.strToInt(tools.getRemainingQuantity()) -1;
+				tools.setRemainingQuantity(remainingQuantity+"");//剩余数量
+				toolsService.updateNotNull(tools);
+				break;
+			default:
+				break;
+			}
+		}
+		return Result.success();
+	}
+
+	@ResponseBody
+	@PostMapping("/update")
+	@RequiresPermissions("failureconfirmation:update")
+	@SystemLog("设备维修故障确认更新")
+	@Override
+	public Result update(@Validated(FailureConfirmation.FailureConfirmationUpdateChecks.class) FailureConfirmation entity) {
+		String curTime  = DateUtil.dateToStr(new Date(), DateUtil.TIME_FORMATE);
+		entity.setUpdateTime(curTime);
+		service.updateNotNull(entity);
+		//故障确认更新设备状态为维修
+		updateAssetStatus(entity);
+		return Result.success();
+	}
+
+	/**
+	 * 故障确认更新设备状态为维修
+	 * @param entity
+	 */
+	private void updateAssetStatus(FailureConfirmation entity){
+		//故障确认，那么设备状态变为维修
+		int isRepair = entity.getRepairRequired();//是否需要维修 1 是 0 否
+		if(isRepair==1){
+			int recordId = entity.getRecordId();
+			MaintenanceRecord record = maintenanceRecordService.queryById(recordId);
+			int assetId = record.getAssetId();
+			Asset asset = new Asset();
+			asset.setId(assetId);
+			asset.setAssetStatus("维修");
+			asset.setAssetStatusCode("asset_status_repair");
+			assetService.updateNotNull(asset);
+		}
+	} 
+
+	@ResponseBody
+	@PostMapping("/delete-batch")
+	@RequiresPermissions("failureconfirmation:delete")
+	@SystemLog("设备维修故障确认删除")
+	@Override
+	public Result deleteBatchByIds(@NotNull @RequestParam("id") Object[] ids) {
+		super.deleteBatchByIds(ids);
+		return Result.success();
+	}
 
 }
